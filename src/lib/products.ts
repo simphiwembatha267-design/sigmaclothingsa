0a2669ee51c2d3512311c058d8e74d56392126fd
Pinnora Base <simphiwembatha267@gmail.com>
2026-08-11 15:21:25 +0200

    Update products.ts

--- src/lib/products.ts
+++ src/lib/products.ts
@@ -12,6 +12,7 @@ import footprintTopBlackBack from '@/assets/sigma-footprint-top-black-back.jpg.a
 import footprintTopWhite from '@/assets/sigma-footprint-top-white.jpg.asset.json';
 import footprintTopWhiteBack from '@/assets/sigma-footprint-top-white-back.jpg.asset.json';
 import empyreanTopBlack from '@/assets/sigma-empyrean-top-white.png';
+import medusaTopBlack from '@/assets/sigma-medusa-noir-top-white.png';
 
 export const products: Product[] = [
   {
